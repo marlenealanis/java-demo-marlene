b5ebc698fa7ac334026dffdfb0a48ad929df852c
marlenealanis <marlenealanis2024@u.northwestern.edu>
2024-02-12 15:23:14 -0800

    made more buttons work

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
+const header = document.querySelector("#header")
+const image = document.querySelector("#image")
+const credit = document.querySelector("#credit")
+
 document.querySelector("#chanterelle-button").addEventListener("click", function() {
-    document.querySelector("#header").innerHTML = "Chanterelle"
-    document.querySelector("#image").src = "chanterelle.jpg"
-    document.querySelector("#credit").innerHTML = "copyright Neil Leclerc"
+    header.innerHTML = "Chanterelle"
+    image.src = "chanterelle.jpg"
+    credit.innerHTML = "copyright Neil Leclerc"
+})
+
+document.querySelector("#oyster-button").addEventListener("click", function() {
+    header.innerHTML = "Oyster mushroom"
+    image.src = "oyster.jpg"
+    credit.innerHTML = "copyright Robbie Sproule"
+})
+
+document.querySelector("#fly-agaric-button").addEventListener("click", function() {
+    header.innerHTML = "Fly agaric"
+    image.src = "fly-agaric.jpg"
+    credit.innerHTML = "copyright Victoria Vasileva"
 })
