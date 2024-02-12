eb0d337fdec242e6372aa6c48244a9c034e59d18
marlenealanis <marlenealanis2024@u.northwestern.edu>
2024-02-12 15:17:43 -0800

    following hilary slides

---
+++ main.js
@@ -0,0 +1,5 @@
+document.querySelector("#chanterelle-button").addEventListener("click", function() {
+    document.querySelector("#header").innerHTML = "Chanterelle"
+    document.querySelector("#image").src = "chanterelle.jpg"
+    document.querySelector("#credit").innerHTML = "copyright Neil Leclerc"
+})
